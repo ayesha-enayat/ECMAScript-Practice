71ad39ee3deeba93d963403fa74d157728c1224e
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-10-25 10:44:02 +0500

    Practice of ES6 Array methods done

--- 03_Class3/script.js
+++ 03_Class3/script.js
@@ -4,23 +4,31 @@
 // let numbers=[1,2,3,4,5,6];
 // numbers.forEach(double);  
 // numbers.forEach(display);
+//console.log(numbers);
 
 // function display(element){
 //     console.log(element);  
 // }
 
+
 // function double(element,index,array){
 //     array[index]=element*2;
 // }
+
+// numbers.forEach(function(element,i,array){
+//     console.log(element,i,array);
+// })
 
 //____________map method() this method make copy of the array
 // const num=[1,2,3,4,5,6,7];
 // const squares=num.map(square);
 // console.log(squares);
+// console.log(num);
 
 // function square(element){
 //     return Math.pow(element,2);
-// }
+// };
+
 
 
 
@@ -33,13 +41,14 @@
 // }
 
 
-//_________________ reduce method() this method reduce the elements of an array to a single value
+//_________________ reduce method() this method reduce the elements of an array to a single value but it doesnot change the original array
 // const prices=[5,30,10,25,15,20];
 // const total=prices.reduce(sum);
 // console.log(total);
+// console.log(prices);
 
 
-// function sum(accumulator,element){
+// function sum(accumulator,element){ //accumulator means previous  element and element means next element
 //     return accumulator+element
 // }
 
@@ -52,7 +61,7 @@
 //     return Math.max(accumulator,element)
 // }
 
-//__________________sort method() 
+//__________________sort method()  //it sort the original array
 
 // let fruits=['apple','orange','coconut','banana'];
 // fruits.sort();
@@ -72,9 +81,12 @@
 // const names=['Ayesha','Aqsa','Iqra'];
 // const foundName=names.find(isAyesha);
 // console.log(foundName);
+// console.log(names);
+
+
 
 // function  isAyesha(element){
-//     return element==='Ayesha';
+//     return element==='Ayesha';  //It return true or false to the function
 // }
 
 // const person=[{name:'Ayesha',age:20},{name:'Aqsa',age:18},{name:'Amna',age:10}]
@@ -121,6 +133,6 @@
 // console.log(num2);
 
 // const numbers=[1,2,3,4,5];
-// numbers.fill(0,1,4);
+// numbers.fill(100,1,4); //(fill value,starting index,endindex-1)
 // console.log(numbers);
 
